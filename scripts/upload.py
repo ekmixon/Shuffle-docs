9baa4e9f533d482d0e6de532decf5a3928664514
Sourcery AI <>
2022-08-02 19:23:07 +0000

    'Refactored by Sourcery'

--- scripts/upload.py
+++ scripts/upload.py
@@ -17,64 +17,60 @@ for dirname in os.listdir(basedir):
         continue
 
     to_upload = []
-    filename = "".join(dirname.split(".")[0:-1])
-    fileread = "%s/%s" % (basedir, dirname)
+    filename = "".join(dirname.split(".")[:-1])
+    fileread = f"{basedir}/{dirname}"
 
     #print("Reading %s" % dirname)
     with open(fileread, "r") as tmp:
         try:
             data = tmp.read().split("\n")
         except UnicodeDecodeError as e:
-            print("Error loading %s: %s" % (dirname, e))
+            print(f"Error loading {dirname}: {e}")
             continue
-    
+
         wrappeditem = {}
         curitem = ""
         for item in data:
             if item.startswith("#"):
-                if curitem:
-                    if wrappeditem["title"] != "Table of contents":
-                        if wrappeditem["ref_url"] not in validurls:
-                            ret = requests.get(wrappeditem["ref_url"])
-                            #print("RET: %d - %s" % (ret.status_code, wrappeditem["ref_url"]))
-                            if ret.status_code != 200:
-                                print("SKIPPING %s (doesn't exist)" % wrappeditem["ref_url"])
-                                break
-                            else:
-                                validurls.append(wrappeditem["ref_url"])
+                if curitem and wrappeditem["title"] != "Table of contents":
+                    if wrappeditem["ref_url"] not in validurls:
+                        ret = requests.get(wrappeditem["ref_url"])
+                        #print("RET: %d - %s" % (ret.status_code, wrappeditem["ref_url"]))
+                        if ret.status_code != 200:
+                            print("SKIPPING %s (doesn't exist)" % wrappeditem["ref_url"])
+                            break
+                        else:
+                            validurls.append(wrappeditem["ref_url"])
 
-                        to_upload.append(wrappeditem)
-                    
+                    to_upload.append(wrappeditem)
+
                 # Priority based on title
                 title = " ".join(item.split("# ")[1:]).strip()
-                priority = 5
-                for char in item:
-                    if char == "#":
-                        priority -= 1
-    
+                priority = 5 - sum(char == "#" for char in item)
                 # Hash is used for prioritizing the search
-                title_hash = hashlib.md5(("%s_%s" % (filename, title)).encode("utf-8")).hexdigest()
+                title_hash = hashlib.md5(f"{filename}_{title}".encode("utf-8")).hexdigest()
                 wrappeditem = {
                     "filename": filename,
                     "title": title.strip(),
                     "data": "",
-                    "url": "https://shuffler.io/docs/%s#%s" % (filename, title.replace(" ", "_").lower()),
-                    "urlpath": "/docs/%s#%s" % (filename, title.replace(" ", "_").lower()),
+                    "url": f'https://shuffler.io/docs/{filename}#{title.replace(" ", "_").lower()}',
+                    "urlpath": f'/docs/{filename}#{title.replace(" ", "_").lower()}',
                     "objectID": title_hash,
                     "priority": priority,
-                    "ref_url": "https://github.com/frikky/shuffle-docs/blob/master/docs/%s.md" % filename,
+                    "ref_url": f"https://github.com/frikky/shuffle-docs/blob/master/docs/{filename}.md",
                 }
+
                 curitem = item
                 continue
-    
+
             if item:
                 curitem += item+"\n"
                 try:
                     wrappeditem["data"] += item
                 except KeyError:
                     wrappeditem["data"] = item
-    
-    if len(to_upload) > 0:
+
+    if to_upload:
         try:
             ret = index.save_objects(to_upload)
             print("%s: %d objects" % (filename, len(to_upload)))
